2057c4c76c0b97a8ee2be8c9cfb7c903bc8c9d6c
Okan <okanaydinhan981@gmail.com>
2025-03-26 19:41:24 +0300

    C-SharpOdevler6
    GenericList

---
+++ acunmedyaOdev26mart/GenericList/GenericList/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Araba
+    {
+        public string marka = null;
+        public string model = null;
+        public double yakitTuketimi = 0.0;
+        public double toplamMesafe = 0.0;
+
+        public Araba(string marka, string model, double yakitTuketimi, double toplamMesafe)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.yakitTuketimi = yakitTuketimi;
+            this.toplamMesafe = toplamMesafe;
+        }
+
+        public double ToplamYakitTuketimiHesapla()
+        {
+            return (toplamMesafe / 100) * yakitTuketimi;
+        }
+    }
+}
---
+++ acunmedyaOdev26mart/GenericList/GenericList/Program.cs
@@ -0,0 +1,19 @@
+﻿using GenericList;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Araba> arabalar = new List<Araba>
+        {
+            new Araba("Toyota", "Corolla", 6.5, 5000),
+            new Araba("BMW", "X5", 9.0, 5000),
+            new Araba("Mercedes", "C180", 8.2, 5000)
+        };
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine("Marka: " + araba.marka + ", Model: " + araba.model + ", 100 km'de Yaktığı Yakıt: " + araba.yakitTuketimi + " litre, Toplam Yakıt Tüketimi: " + araba.ToplamYakitTuketimiHesapla() + " litre");
+        }
+    }
+}
